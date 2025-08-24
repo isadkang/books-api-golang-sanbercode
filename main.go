ed9ccb714f9cd1880b6751db66b0a82db9834a2e
isadkang <irsyadrecovery15@gmail.com>
2025-08-24 21:30:59 +0700

    tes

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"books-api/config"
 	"books-api/routes"
+	"os"
 )
 
 func main() {
 	config.InitDB()
 	config.RunMigrations()
 	r := routes.SetupRouter()
-	r.Run(":8080")
+	r.Run(":" + os.Getenv("PORT"))
 }
